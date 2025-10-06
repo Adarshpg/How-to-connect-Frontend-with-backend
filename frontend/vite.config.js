73c3f6d46386a96cf2cf8704c2d5c6b5000b0207
Adarsh Gayadolli <137860068+Adarshpg@users.noreply.github.com>
2025-10-06 10:31:52 +0530

    url is changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://how-to-connect-frontend-with-backend.onrender.com'
+      '/api':'http://localhost:3000'
     },
   },
   plugins: [react()],
